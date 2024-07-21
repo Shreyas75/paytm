e4c22bdf6b0c4fefbb0c9dd7612ba520c682f4bf
Shreyas75 <manotishreyas@gmail.com>
2024-07-21 08:12:48 +0530

    Balance Updated

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,12 +1,27 @@
-export const Balance = ({ value }) => {
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-    // axios.get("http://localhost:3000/api/v1/account/balance", )
-    return <div className="flex">
-        <div className="font-bold text-lg">
-            Your balance
+export const Balance = ( ) => {
+    const [balance, setBalance] = useState(0);
+
+    
+    useEffect( () => {
+        console.log("hi")
+        const response = axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+            }
+        }).then((response) => {
+            setBalance(response.data.balance);
+        })
+    }, [])
+    return (
+        <div className="flex">
+            <div className="font-bold text-lg">
+                Your balance
+            </div>
+            <div className="font-semibold ml-4 text-lg">
+                $ {balance}
+            </div>
         </div>
-        <div className="font-semibold ml-4 text-lg">
-            Rs {value}
-        </div>
-    </div>
-}
+)}
